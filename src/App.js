cab4d7a39bdb7ada9ee759fd0fb67ec3b6078d42
Emily Hosoya <Emily@Noriakis-MacBook-Pro.local>
2020-11-04 13:46:43 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Grocery List Application</h1>
+        <ul>
+          <li>Strawberries</li>
+          <li>Eggs</li>
+          <li>Milk</li>
+        </ul>
       </header>
     </div>
   );
